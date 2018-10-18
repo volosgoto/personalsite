37cb4b47ef137f8fcfd2aefb26783210ab221a3a
volosgoto <volosgoto@yahoo.com>
2018-10-18 15:19:39 +0300

    navbar

---
+++ app/src/myMain.js
@@ -0,0 +1,11 @@
+// alert('RERE');
+
+
+function navbarCollapse() {
+  var x = document.getElementById("navbar");
+  if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+  } else { 
+      x.className = x.className.replace(" w3-show", "");
+  }
+}
