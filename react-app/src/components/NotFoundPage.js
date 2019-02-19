137c0338d92144fa7c0f25a78117ad220fcbab3c
volosgoto <volosgoto@yahoo.com>
2019-02-19 16:52:36 +0200

    add 404

--- react-app/src/components/NotFoundPage.js
+++ react-app/src/components/NotFoundPage.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import NotFound from "../images/404_page.png";
 
 const NotFoundPage = () => (
-  <div>
-    404 - <Link to="/">Go home</Link>
+  <div className="container">
+    <h1>404. Page not found</h1>
+    <h2>
+      <Link to="/">Go home</Link>
+    </h2>
+
+    <div className="container">
+      <div className="w3-card w3-hover-shadow w3-hover-opacity w3-center w3-margin">
+        <img src={NotFound} width="400px" />
+      </div>
+    </div>
   </div>
 );
 
