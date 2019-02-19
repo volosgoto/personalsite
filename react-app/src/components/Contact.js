85870f623c0a806c3229be0a8e2e61d211f05657
volosgoto <volosgoto@yahoo.com>
2019-02-19 15:45:44 +0200

    add mailto

--- react-app/src/components/Contact.js
+++ react-app/src/components/Contact.js
@@ -1,60 +1,53 @@
-
 // Convert Contact to class component
 
+import React from "react";
+import Mailto from "react-protected-mailto";
 
-import React from 'react'
-
-let Contact = (props) => {
-
-
-  // function navbarCollapse() {
-  //   var x = document.getElementById("navbar");
-  //   if (x.className.indexOf("w3-show") == -1) {
-  //     x.className += " w3-show";
-  //   } else {
-  //     x.className = x.className.replace(" w3-show", "");
-  //   }
-  // }
-
-
-
-
-
-
+let Contact = props => {
   return (
     <div className="container">
       <div className="style-container">
-        <div className="style-box-1"><h3>ANDRII VOLOSOVYCH</h3></div>
         <div className="style-box-1">
-          <p id="contacts">Phone: +38 (063) 871-51-51</p>
+          <h3>ANDRII VOLOSOVYCH</h3>
+        </div>
+        <div className="style-box-1">
+          <p id="contacts">
+            Phone: +38 (063) 871-51-51
+            <p>
+              Email:{" "}
+              <Mailto
+                email="volosovich@i.ua"
+                headers={
+                  ({ subject: "Question from Volosovich A. personal website" },
+                  { cc: "" })
+                }
+              />
+            </p>
+          </p>
 
           {/* react-mailto */}
           {/* https://www.npmjs.com/package/react-mailto */}
 
           {/* <a href="location.href = 'mailto:volosovich@i.ua'">E-mail: volosovich@i.ua</a> */}
-          <p></p>
-          <a href="https://join.skype.com/invite/bXprd7VcNmnf">Skype: volosovich82</a>
+          <p />
+          <a href="https://join.skype.com/invite/bXprd7VcNmnf">
+            Skype: volosovich82
+          </a>
           <p>Address: Kyiv, Ukraine</p>
           <p>Phone: +38 (063) 871-51-51</p>
-
         </div>
       </div>
 
       <div className="style-box-1">
-
-
-
         {/* Iframe */}
         {/* https://www.npmjs.com/package/react-iframe */}
-
 
         {/* <iframe id="google_map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5082.447956365541!2d30.3780815!3d50.4369286!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cb87efca634b%3A0xe934b78f305f2489!2zNTDCsDI2JzExLjAiTiAzMMKwMjInNTcuMCJF!5e0!3m2!1sru!2sua!4v1540384556782" width="595" height="450" frameBorder="0" style="border:0" allowFullScreen>
         </iframe> */}
       </div>
-
     </div>
-  )
-}
+  );
+};
 
 // function mailTo() {
 //   return location.href = 'mailto:volosovich@i.ua';
