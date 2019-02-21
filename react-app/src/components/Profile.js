52ba3ce76aeb2592e51ce30e75c80bb7b1bc7814
volosgoto <volosgoto@yahoo.com>
2019-02-21 15:47:03 +0200

    add props to Profile

--- react-app/src/components/Profile.js
+++ react-app/src/components/Profile.js
@@ -1,22 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
 import ProfileImg from "../images/andriivolosovych.jpeg";
 
-export default function Profile() {
+function Profile(props) {
+  props = {
+    name: "Andrii Volosovych",
+    image: ProfileImg,
+    jobs: "Frond-End, JavaScript, React, PHP",
+    vocation: "Developer"
+  };
+  const { name, image, jobs, vocation } = props;
   return (
     <div className="w3-col w3-quarter">
       <div className="w3-container w3-mobile w3-center">
-        <h3>ANDRII VOLOSOVYCH</h3>
-        <h3>User Name</h3>
+        <h3>{name}</h3>
         <div className="w3-card w3-hover-shadow w3-hover-opacity w3-center">
-          <img src={ProfileImg} alt="ProfileImg" style={{ width: "100%" }} />
+          <img src={image} alt="profile_image" style={{ width: "100%" }} />
           <div className="w3-container">
             <h4>
-              <b>Frond-End, JavaScript, React, PHP</b>
+              <b>{jobs}</b>
             </h4>
-            <p>Developer</p>
+            <p>{vocation}</p>
           </div>
         </div>
       </div>
     </div>
   );
 }
+
+// class Profile extends Component {
+//   state = {
+//     name: "Andrii Volosovych",
+//     image: ProfileImg,
+//     jobs: "Frond-End, JavaScript, React, PHP",
+//     vocation: "Developer"
+//   };
+//   render() {
+//     const { name, image, jobs, vocation } = this.state;
+//     return (
+//       <div className="w3-col w3-quarter">
+//         <div className="w3-container w3-mobile w3-center">
+//           <h3>{name}</h3>
+//           <div className="w3-card w3-hover-shadow w3-hover-opacity w3-center">
+//             <img src={image} alt="profile_image" style={{ width: "100%" }} />
+//             <div className="w3-container">
+//               <h4>
+//                 <b>{jobs}</b>
+//               </h4>
+//               <p>{vocation}</p>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+export default Profile;
