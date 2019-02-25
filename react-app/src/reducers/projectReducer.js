ed0ee4a1e474efc03ce1ba40e451ef5158b94e52
volosgoto <volosgoto@yahoo.com>
2019-02-25 12:59:41 +0200

    set weather projeckt

--- react-app/src/reducers/projectReducer.js
+++ react-app/src/reducers/projectReducer.js
@@ -1,6 +1,6 @@
-import imagesAsset from '../assets/imagesAsset';
-import { GET_PROJECTS } from '../actions/types';
-import uuid from 'uuid';
+import imagesAsset from "../assets/imagesAsset";
+import { GET_PROJECTS } from "../actions/types";
+import uuid from "uuid";
 
 const {
   GitHubFinderImg,
@@ -18,23 +18,43 @@ const initialState = {
   projects: [
     {
       id: uuid(),
-      projectName: 'GitHub Finder',
-      projectLink: "../modules/GitHub_Finder-master/index.html",
+      projectName: "GitHub Finder",
+      projectLink: "https://volosgoto.github.io/GitHub_Finder/",
       image: GitHubFinderImg,
-      linkTitle: 'Try it'
+      linkTitle: "Try it"
     },
     {
       id: uuid(),
-      projectName: 'Add_Book_JS-master',
-      projectLink: '/app/src/GitHub_Finder-master/index.html',
+      projectName: "Add_Book_JS",
+      projectLink: "https://volosgoto.github.io/Add_Book_JS/",
       image: AddBookJSMasterImg,
-      linkTitle: 'Try it'
+      linkTitle: "Try it"
+    },
+    {
+      id: uuid(),
+      projectName: "JS_FETCH_API",
+      projectLink: "",
+      image: JSFetchAPIMasterImg,
+      linkTitle: "Try it"
+    },
+    {
+      id: uuid(),
+      projectName: "JS_Form_Validation",
+      projectLink: "https://volosgoto.github.io/JS_Form_Validation/",
+      image: JSFormValidationMasterImg,
+      linkTitle: "Try it"
+    },
+    {
+      id: uuid(),
+      projectName: "JS_Weather",
+      projectLink: "https://volosgoto.github.io/JS_Weather/",
+      image: JSWeatherImg,
+      linkTitle: "Try it"
     }
   ]
-
 };
 
-export default function (state = initialState, action) {
+export default function(state = initialState, action) {
   switch (action.type) {
     case GET_PROJECTS: {
       return { ...state };
