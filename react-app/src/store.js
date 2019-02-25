a3c48ea40c33dbb1ab6e4eae2ca8609bbd4d7731
andrey <volosgoto@yahoo.com>
2019-02-25 21:32:56 +0200

    set htaccess

--- react-app/src/store.js
+++ react-app/src/store.js
@@ -5,15 +5,15 @@ import rootReducer from './reducers';
 // Init state
 const initialState = {};
 
-const middleware = [thunk];
+// const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  // compose(
+  //   applyMiddleware(...middleware),
+  //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // )
 );
 
 export default store;
