33b3086c6ee52006f038c4ffe0936e166d4a26c8
andrey <volosgoto@yahoo.com>
2020-01-11 16:08:42 +0200

    404

--- react-app/src/routers/AppRouter.js
+++ react-app/src/routers/AppRouter.js
@@ -1,40 +1,33 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Header from '../components/layouts/Header';
-import Footer from '../components/layouts/Footer';
-import NotFoundPage from '../components/layouts/NotFoundPage';
-import App from '../components/App';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "../components/layouts/Header";
+import Footer from "../components/layouts/Footer";
+import NotFoundPage from "../components/layouts/NotFoundPage";
+import App from "../components/App";
 
+import Resume from "../components/pages/Resume";
+import Contact from "../components/pages/Contact";
+import About from "../components/pages/About";
 
-import Resume from '../components/pages/Resume';
-import Contact from '../components/pages/Contact';
-import About from '../components/pages/About';
+import GitHubFinder from "../modules/GitHub_Finder-master/GitHubFinder";
 
-import GitHubFinder from '../modules/GitHub_Finder-master/GitHubFinder';
-
-import { Provider } from 'react-redux';
-import store from '../store';
-
+import { Provider } from "react-redux";
+import store from "../store";
 
 const AppRouter = () => (
   <Provider store={store}>
     <BrowserRouter>
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={App} />
-          <Route path="/resume" component={Resume} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/about" component={About} />
-          <Route path="/modules/GitHub_Finder-master/index.html" component={GitHubFinder} />
-          <Route component={NotFoundPage} />
-        </Switch>
-        <Footer />
-      </div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/resume" component={Resume} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/about" component={About} />
+        <Route component={NotFoundPage} />
+      </Switch>
+      <Footer />
     </BrowserRouter>
   </Provider>
 );
 
 export default AppRouter;
-
-
