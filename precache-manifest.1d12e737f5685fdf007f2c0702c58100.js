4f931163a09ed6e00e447b33d46781964c42193c
andrey <volosgoto@yahoo.com>
2020-01-11 15:32:32 +0200

    Updates

---
+++ precache-manifest.1d12e737f5685fdf007f2c0702c58100.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0fab1fa052d3c461fa74c68cea95e7c",
+    "url": "/personalsite/index.html"
+  },
+  {
+    "revision": "a5b81645cdaaf3424275",
+    "url": "/personalsite/static/css/2.56f7e923.chunk.css"
+  },
+  {
+    "revision": "8ea9ffc82bf0ee2b1768",
+    "url": "/personalsite/static/css/main.b1fcde25.chunk.css"
+  },
+  {
+    "revision": "a5b81645cdaaf3424275",
+    "url": "/personalsite/static/js/2.d8e94ff6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/personalsite/static/js/2.d8e94ff6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ea9ffc82bf0ee2b1768",
+    "url": "/personalsite/static/js/main.1cc2233b.chunk.js"
+  },
+  {
+    "revision": "c2d3502a000d50781041",
+    "url": "/personalsite/static/js/runtime-main.402a00b0.js"
+  },
+  {
+    "revision": "d0d45cd117e94fb3b0b04e3dc884cb16",
+    "url": "/personalsite/static/media/404_page.d0d45cd1.png"
+  },
+  {
+    "revision": "4e3828c8ed425edeb685065f6a00dc7f",
+    "url": "/personalsite/static/media/Add Book.4e3828c8.png"
+  },
+  {
+    "revision": "fcbefbeb803830b38a4203c541e184a6",
+    "url": "/personalsite/static/media/Cert_Java_OOP_Volosovych_Andrii.fcbefbeb.png"
+  },
+  {
+    "revision": "dff85f9a92c5c70a65a37464ca81d866",
+    "url": "/personalsite/static/media/Cert_Java_Start_Volosovych_Andrii.dff85f9a.png"
+  },
+  {
+    "revision": "9d9b85b52dd3a0c93080996eb79a64fd",
+    "url": "/personalsite/static/media/Cert_PHP_Academy_Volosovych_Andrii.9d9b85b5.jpg"
+  },
+  {
+    "revision": "58952846f413bdba6b00e4c48a713437",
+    "url": "/personalsite/static/media/ClientPanel.58952846.png"
+  },
+  {
+    "revision": "7973b23c1b477201609243a093d64dd4",
+    "url": "/personalsite/static/media/ContactList.7973b23c.png"
+  },
+  {
+    "revision": "7c9017423aca7d4ad65274488af3c17f",
+    "url": "/personalsite/static/media/GitHub Finder.7c901742.png"
+  },
+  {
+    "revision": "23b6cb9b1d300bd578777801429725ad",
+    "url": "/personalsite/static/media/JS_FETCH_API-master.23b6cb9b.png"
+  },
+  {
+    "revision": "3022a2ada2c3ba0392e3c4296e5f5d7b",
+    "url": "/personalsite/static/media/JS_Form_Validation-master.3022a2ad.png"
+  },
+  {
+    "revision": "7474d5897427aee3924bdcc0b9e416fc",
+    "url": "/personalsite/static/media/JS_Weather.7474d589.png"
+  },
+  {
+    "revision": "9d6c26933821db23565645adbabeea00",
+    "url": "/personalsite/static/media/Yii_shop.9d6c2693.png"
+  },
+  {
+    "revision": "0e98a235fba4f08007f495b96cf81f8a",
+    "url": "/personalsite/static/media/andriivolosovych.0e98a235.jpeg"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/personalsite/static/media/fa-brands-400.088a34f7.eot"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/personalsite/static/media/fa-brands-400.273dc9bf.ttf"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/personalsite/static/media/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "d72293118cda50ec50c39957d9d836d0",
+    "url": "/personalsite/static/media/fa-brands-400.d7229311.svg"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/personalsite/static/media/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/personalsite/static/media/fa-regular-400.3ac49cb3.eot"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/personalsite/static/media/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/personalsite/static/media/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "d2e53334c22a9a4937bc26e84b36e1e0",
+    "url": "/personalsite/static/media/fa-regular-400.d2e53334.svg"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/personalsite/static/media/fa-regular-400.ece54318.ttf"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/personalsite/static/media/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "/personalsite/static/media/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/personalsite/static/media/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/personalsite/static/media/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/personalsite/static/media/fa-solid-900.f6121be5.woff2"
+  },
+  {
+    "revision": "1bafa9b6efb906b5ac18ef944e4fbe5c",
+    "url": "/personalsite/static/media/synprotech.1bafa9b6.png"
+  },
+  {
+    "revision": "0c8e63b1ec41bec36aefc0856004c67b",
+    "url": "/personalsite/static/media/yii_blog.0c8e63b1.png"
+  }
+]);
